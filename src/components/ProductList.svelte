074bd7f4ca5027b8137c15a313455c55cafc3e87
Enoch Olayemi <enocholayemi928@gmail.com>
2026-06-04 10:31:29 -0600

    completed team activity 5 things.

--- src/components/ProductList.svelte
+++ src/components/ProductList.svelte
@@ -3,15 +3,23 @@
   import { getProducts } from "../js/productData.mts";
   import type { Product } from "../js/types.mts";
   import { getParam } from "../js/utils.mjs";
+  import ProductSummary from "./ProductSummary.svelte";
 
 // declare these out here as state so we can us it in our template below
   let category = $state(""); 
-  let products:Product[] = $state([]);
+  let products: Product[] = $state([]);
+  let loading = $state(true);
 
   async function init() {
-    category = getParam("category") || ""
-    const data = await getProducts(category);
-    products = data.results;
+    category = getParam("category") || "tents";
+    try {
+      const data = await getProducts(category);
+      products = data.results || data;;
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    } finally {
+      loading = false;
+    }
   }
 
   onMount(init);
@@ -19,18 +27,14 @@
 
 <h2>Top products: {category}</h2>
 
-<ul class="product-list">
-{#each products as product}
-<li class="product-card">
-        <a href="products/880RR/">
-          <img
-            src="images/tents/marmot-ajax-tent-3-person-3-season-in-pale-pumpkin-terracotta~p~880rr_01~320.jpg"
-            alt="Marmot Ajax tent"
-          />
-          <h3 class="card__brand">Marmot</h3>
-          <h2 class="card__name">Ajax Tent - 3-Person, 3-Season</h2>
-          <p class="product-card__price">$199.99</p></a
-        >
-      </li>
-{/each}
-</ul>  
+{#if loading}
+  <p>Loading products...</p>
+{:else if products.length === 0}
+  <p>No products found for this category.</p>
+{:else}
+  <ul class="product-list">
+    {#each products as product}
+      <ProductSummary {product} />
+    {/each}
+  </ul>
+{/if}
--- src/components/ProductSummary.svelte
+++ src/components/ProductSummary.svelte
@@ -4,3 +4,12 @@
 </script>
 
 <h2>Production Summary</h2>
+<li class="product-card">
+<a href="/products/{product.id}">
+  <img src={product.images.primaryLarge} alt={product.name} />
+  <div class="product-info">
+    <h3>{product.name}</h3>
+    <p>${product.finalPrice.toFixed(2)}</p>
+  </div>
+</a>
+</li>
