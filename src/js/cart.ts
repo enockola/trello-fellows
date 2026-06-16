f3e91f49d3d9ca3dd9d4511bf10760bfe299cd4b
ButterMyLoaf23 <bostonwyatt1@gmail.com>
2026-06-16 09:50:01 -0700

    css and total to cart changes

--- src/js/cart.ts
+++ src/js/cart.ts
@@ -6,13 +6,28 @@ function renderCartContents() {
   const htmlItems = cartItems.map((item: Product) => cartItemTemplate(item));
   const listEl = document.querySelector(".product-list");
   if (listEl) listEl.innerHTML = htmlItems.join("");
+
+  if (cartItems.length > 0) {
+  const footer = document.querySelector(".cart-footer");
+  footer?.classList.remove("hide");
+
+  const total = cartItems.reduce((sum: number, item: Product) => sum + item.finalPrice, 0);
+
+    const totalElement = document.querySelector(".cart-total");
+
+    if (totalElement) {
+
+      totalElement.innerHTML = `Total: $${total.toFixed(2)}`;
+    }
+  }
 }
+
 
 function cartItemTemplate(item: Product) {
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
-      src="${item.images.primaryExtraLarge}"
+      src="${item.image}"
       alt="${item.name}"
     />
   </a>
